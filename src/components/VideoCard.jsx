75139878d10c0e13b4acbe116b45b3a27f8d7590
nazimboudeffa <boudeffa@gmail.com>
2023-06-17 13:12:34 +0200

    Little fix

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -82,9 +82,9 @@ const VideoCard = ({ video }) => {
               </div>
             </div>
             <div className="flex justify-between">
-              <div className="bg-red-600 mt-3">
+              <div className="bg-red-600 mt-3 w-20">
                 <div className="flex pl-2 pr-3 pt-1 pb-1 flex-row">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-white mr-1">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                   </svg>
@@ -93,7 +93,7 @@ const VideoCard = ({ video }) => {
                   </span>
                 </div>
               </div>
-              <div className="mt-4 mr-1">
+              <div className="mt-4">
                 {video.types?.map((type) => {return (<span className="rounded-xl border-2 border-zinc-900 bg-zinc-900 p-1 text-xs mr-1">{type}</span>)})}
               </div>
             </div>
