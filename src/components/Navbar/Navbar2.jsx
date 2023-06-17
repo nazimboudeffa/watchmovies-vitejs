95fa6b71c169796e09c7883470c7e6ea2e11f3d2
nazimboudeffa <boudeffa@gmail.com>
2023-06-17 17:11:25 +0200

    Added TIP

--- src/components/Navbar/Navbar2.jsx
+++ src/components/Navbar/Navbar2.jsx
@@ -152,26 +152,39 @@ const Navbar2 = () => {
                 
               </ul>
             </nav>
-
-            <div className="block md:hidden">
-              <button
-                className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
+            <div className="flex items-center gap-4">
+              <div className="sm:flex sm:gap-4">
+                <NavLink
+                  to="https://fr.tipeee.com/nazimboudeffa"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
+                  <img
+                    src="tipeee_tip_btn.svg"
+                    alt="tip"
+                    height={45}
+                    width={45} 
                   />
-                </svg>
-              </button>
+                </NavLink>
+              </div>
+              <div className="block md:hidden">
+                <button
+                  className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                </button>
+              </div>
             </div>
           </div>
         </div>
